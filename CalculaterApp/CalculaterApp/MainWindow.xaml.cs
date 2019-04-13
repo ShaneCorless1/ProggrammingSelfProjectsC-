3c3097710dd3dbfcc998ac2e5b07918ce2a4b598
shane <shane@DESKTOP-9C8J1RH>
2019-04-13 19:54:28 +0100

    Few changes

--- CalculaterApp/CalculaterApp/MainWindow.xaml.cs
+++ CalculaterApp/CalculaterApp/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace CalculaterApp
     {
         //Variables to store the last number and the result
         double lastnumber, result;
+        SelectedOperater SelectedOperater;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,12 +35,72 @@ namespace CalculaterApp
 
         }
 
+        // Methods to code the buttons
 
-        private void Sevenbutton_Click(object sender, RoutedEventArgs e)
+
+        private void Operation_Button(object sender, RoutedEventArgs e)
         {
+        
+                if (double.TryParse(resultlabel.Content.ToString(), out lastnumber))
+                {
+                  
+                    resultlabel.Content ="0";
+                }
+
+                if (sender == Multiplicationbutton)
+                {
+                    SelectedOperater = SelectedOperater.Multiplication;
+                }
+                else if (sender == DivisionButton)
+                {
+                SelectedOperater = SelectedOperater.Division;
+                }
+                else if (sender == Addbutton )
+                {
+                SelectedOperater = SelectedOperater.Addition;
+                }
+                else if (sender == Subtractbutton)
+                {
+                SelectedOperater = SelectedOperater.Subtraction;
+
+                }
+
+            
+            
+
+            
+        }
+        private void Numberbutton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender ==Zerobutton)
+            selectedValue = 0;
+            else if (sender == Onebutton)
+            selectedValue = 1;
+            else if (sender == Twobutton)
+            selectedValue = 2;
+            else if (sender == Threebutton)
+            selectedValue = 3;
+            else if (sender == Fourbutton)
+            selectedValue = 4;
+            else if (sender == Fivebutton)
+            selectedValue = 5;
+            else if (sender == Sixbutton)
+            selectedValue = 6;
+            else if (sender == sevenbutton)
+            selectedValue = 7;
+            else if (sender == Eightbutton)
+            selectedValue = 8;
+            else if (sender == Ninebutton)
+            selectedValue = 9;
+            
+
+
+
             if (resultlabel.Content.ToString() =="0" )
             {
-                resultlabel.Content = "7";
+                resultlabel.Content = $"{selectedValue}";
             }
             else
             {
@@ -48,21 +110,31 @@ namespace CalculaterApp
 
         }
 
-        private void Twobutton_Click(object sender, RoutedEventArgs e)
-        {
-            if (resultlabel.Content.ToString() =="0")
-            {
-                resultlabel.Content = "2";
-            }
-            else
-            {
-                resultlabel.Content = $"{resultlabel.Content}2";
-            }
-        }
+     
 
         private void Equalsbutton_Click(object sender, RoutedEventArgs e)
         {
+            double temporaryNumber = 0;
 
+            if (double.TryParse(resultlabel.Content.ToString(), out lastnumber))
+            {
+                switch (SelectedOperater)
+                {
+                    case SelectedOperater.Addition:
+                        result = SimpleMath.Add(lastnumber, temporaryNumber);
+                        break;
+                    case SelectedOperater.Subtraction:
+                        result = SimpleMath.Subtraction(lastnumber, temporaryNumber);
+                        break;
+                    case SelectedOperater.Multiplication:
+                        result = SimpleMath.Multiply(lastnumber, temporaryNumber);
+                        break;
+                    case SelectedOperater.Division:
+                        result = SimpleMath.Division(lastnumber, temporaryNumber);
+                        break;
+                                    }
+            }
+            resultlabel.Content = temporaryNumber.ToString();
         }
 
         private void Addbutton_Click(object sender, RoutedEventArgs e)
@@ -74,13 +146,25 @@ namespace CalculaterApp
         {
             resultlabel.Content = 0;
         }
-
+      
         private void Percentagebutton_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(resultlabel.Content.ToString(), out lastnumber))
             {
                 lastnumber = lastnumber / 100;
                 resultlabel.Content = lastnumber.ToString();
+            }
+        }
+
+        private void Decimalbutton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultlabel.Content.ToString().Contains("."))
+            {
+                resultlabel.Content = $"{resultlabel.Content}";
+            }
+            else
+            {
+                resultlabel.Content = $"{resultlabel.Content}.";
             }
         }
 
@@ -92,5 +176,35 @@ namespace CalculaterApp
                 resultlabel.Content = lastnumber.ToString();
             }
         }
+    }
+
+    public enum SelectedOperater
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public static double Subtraction(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+        public static double Multiply(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double Division(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
     }
 }
