a8b10f0d6516fad45c5ce6dd162cd0a3339bba3f
shane <shane@DESKTOP-9C8J1RH>
2019-04-13 20:24:54 +0100

    Creating a contact application and connecting it to a SQLite Database

---
+++ ContactsApp/WpfApp1/Classes/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLitePCL;
+namespace WpfApp1
+{
+    [Table("Contact")]
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
